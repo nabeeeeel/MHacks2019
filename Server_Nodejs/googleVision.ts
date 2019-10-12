7fbc9e86248554edcf35f5897f969c523666230e
Brehm <brehm@Brehms-MacBook-Pro.local>
2019-10-12 07:43:24 -0400

    yeeted google vision

---
+++ Server_Nodejs/googleVision.ts
@@ -0,0 +1,9 @@
+import {google} from 'googleapis';
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = "/Users/brehm/MHacks2019/serviceaccount.json";
+
+function callGoogleVision(filepath:string){
+
+    
+
+}
