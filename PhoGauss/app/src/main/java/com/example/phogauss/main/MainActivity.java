0908e491ae2c02275a194f03b2d140b130d22288
Nabeel <nsahmed@protonmail.com>
2019-10-12 19:33:23 -0400

    Photo Retrieval
    Can search for image and add it to app

--- PhoGauss/app/src/main/java/com/example/phogauss/main/MainActivity.java
+++ PhoGauss/app/src/main/java/com/example/phogauss/main/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.phogauss.main;
 
-import android.os.Bundle;
+;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.phogauss.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    final int RESULT_LOAD_IMAGE = 1;
+    ImageView imageView;
+    Bitmap bitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +35,16 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton cameraFab = findViewById(R.id.cameraFAB);
         FloatingActionButton galleryFab = findViewById(R.id.galleryFAB);
-
+        imageView = findViewById(R.id.imageView);
         cameraFab.setOnClickListener( view -> {
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Select Image", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
+
+            Intent i = new Intent(
+                    Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            startActivityForResult(i, RESULT_LOAD_IMAGE);
         });
 
         galleryFab.setOnClickListener( view -> {
@@ -28,11 +52,43 @@ public class MainActivity extends AppCompatActivity {
                     .setAction("Action", null).show();
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
 
 
 
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
+            if (bitmap != null) {
+                convertBitMap(bitmap);
+            }
+        }
+    }
+        void convertBitMap(Bitmap bitmap) {
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+        }
 
     }
 
-}
+ //   }
+
